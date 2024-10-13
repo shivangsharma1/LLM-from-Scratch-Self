18a131e9608c39ab9f12d5311ad7b27557c592a1
shivangsharma1 <shivi97sharma@gmail.com>
2024-10-13 07:58:38 +0530

    efficient attention

---
+++ Chapter 3/mha_efficient.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+torch.manual_seed(123)
+
+
+class MultiheadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_len, num_heads, dropout=0.5, bias=False):
+        super().__init__()
+
+        # to split the d_out dim in num_head for parallel processing
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+        self.num_head = num_heads
+        self.head_dim = d_out // num_heads
+        self.d_out = d_out
+        self.q = nn.Linear(d_in, d_out, bias=bias)
+        self.k = nn.Linear(d_in, d_out, bias=bias)
+        self.v = nn.Linear(d_in, d_out, bias=bias)
+        self.dropout = nn.Dropout(dropout)
+
+        # linear layer to combine heads output
+        self.out_proj = nn.Linear(d_out, d_out, bias=bias)
+
+        # declaration for attention calculation, defining it as register_buffer so that it moves to \
+        # model current device
+        self.register_buffer("mask", torch.triu(torch.ones(context_len, context_len), diagonal=1)) #mask
+
+    def forward(self, x):
+        b, token_len, d_in = x.shape
+        query = self.q(x)
+        key = self.k(x)
+        val = self.v(x)
+
+        #converting into MHA dim
+        key = key.view(b, token_len, self.num_head, self.head_dim)
+        query = query.view(b, token_len, self.num_head, self.head_dim)
+        val = val.view(b, token_len, self.num_head, self.head_dim)
+
+        # converting to channel dim
+        key = key.transpose(2, 3)
+        query = query.transpose(2, 3) 
+        val = val.transpose(2, 3)
+
+        #normal attention calculation
+        attn_score = query @ key.transpose(2, 3)
+        mask_bool = self.mask.bool()[:token_len, :token_len]
+
+        #masking
+        atten_weight.masked_fill_(mask_bool, -torch.inf)
+
+        atten_weight = torch.softmax(attn_score / (key.shape[-1] ** 0.5), dim = -1)
+        atten_weight = self.dropout(atten_weight)
+
+        context_vec = (atten_weight @ val).transpose(1, 2)
+        context_vec = context_vec.contiguous().view(b, token_len, self.d_out)
+
+        #passing the vector to output projection layer to combine the heads output, so this is also a trainable layer
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+    
+
+
+
+if __name__ == "__main__":
+    d_in, d_out = 3, 8
+
+    x_in = torch.rand(5, 3)
+    batch = torch.stack((x_in, x_in), dim=0)
+
+    print("batch size", batch.shape)
+
+    # masked attention
+    mha = MultiheadAttention(d_in, d_out, batch.shape[1], num_heads=2)
+    print("Causal", mha.forward(batch))
+    print("Output shape", mha.forward(batch).shape)
